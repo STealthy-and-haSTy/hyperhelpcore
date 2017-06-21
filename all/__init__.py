56902792b2a7fdd25bd64781e9e98a63db2ee348
Terence Martin <github@nurdz.com>
2017-06-20 21:06:29 -0700

    Include a package version number
    This includes in the core package the concept of a version number that
    underlying code could use to determine what version of the core it is
    interfacing with.
    
    This is only really needed for packages that get at the underlying
    core code in hyperhelp, which at the moment would only be the
    companion HyperHelpAuthor package.
    
    To this end (as an experiment) the code for loading in the help index
    files is exposed to anyone that wants to import it as a test for how
    this will eventually work. In particular, the idea is to put all of
    the symbols meant to be accessible to outside code into the
    hyperhelp.all module namespace (whicn is unfortunate but there seems
    to be no satisfactory way around it).

--- all/__init__.py
+++ all/__init__.py
@@ -1,8 +1,27 @@
 ###----------------------------------------------------------------------------
 
+__version_tuple = (1, 0, 0)
+__version__ = ".".join([str(num) for num in __version_tuple])
 
+
+# These are exposed to Sublime to implement the core of the help system.
 from .help import HyperHelpCommand, HyperHelpNavigateCommand
 from .help import HyperHelpListener
+
+
+# These are exposed to packages that may want to interface with the hyperhelp
+# core for use in their own packages.
+from .operations import package_help_scan
+
+
+###----------------------------------------------------------------------------
+
+
+def version():
+    """
+    Get the currently installed version of hyperhelp as a tuple.
+    """
+    return __version_tuple
 
 
 ###----------------------------------------------------------------------------
