a90d45e93d56e3da1bc6a38d3180145fff37c10e
Terence Martin <github@nurdz.com>
2017-08-11 15:51:02 -0700

    Cosmetic cleanup of imports
    What it says on the tin; this is of no great consequence one way or
    the other but the extremely slight reordering of a single item makes
    this look nicer to my eye.
    
    Also I'm procrastinating on rewriting the help.

--- all/__init__.py
+++ all/__init__.py
@@ -12,8 +12,8 @@ from .help import HyperHelpListener
 # These functions represent the API that is exposed to external packages that
 # may want/need to get at some of the internals of hyperhelp.
 from .operations import HelpData, HeaderData, HistoryData
-from .operations import scan_packages, reload_package, parse_header
-from .operations import help_index_list
+from .operations import scan_packages, reload_package
+from .operations import parse_header, help_index_list
 from .operations import help_view, focus_on, display_help, reload_help
 from .operations import show_topic, navigate_history
 
