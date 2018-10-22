100e644746c96895f26ef34f4a6254357254f7d7
Terence Martin <github@nurdz.com>
2018-10-22 16:19:22 -0700

    Change the _hh_links setting structure
    Where previously this was a dictionary where the index was a region
    start position (encoded as a string) and the value was the topic data
    dictionary, the setting now uses a region index instead of position.
    
    The general idea is that although the position of the region may change,
    the location of the region in the buffer does not.
    
    This makes link navigation more robust in cases where the text of the
    loaded help file gets modified after links are post processed.

--- internalcmd.py
+++ internalcmd.py
@@ -129,7 +129,7 @@ class HyperhelpInternalProcessLinksCommand(sublime_plugin.TextCommand):
 
         default_pkg = current_help_package(self.view)
 
-        tmp = {}
+        hh_links = {}
         regions = v.get_regions("_hh_links")
         for idx,region in enumerate(reversed(regions)):
             base_text = v.substr(region)
@@ -141,16 +141,10 @@ class HyperhelpInternalProcessLinksCommand(sublime_plugin.TextCommand):
                 text = base_text
 
             v.replace(edit, region, text)
-            tmp[len(regions) - idx - 1] = {
+            hh_links[str(len(regions) - idx - 1)] = {
                 "pkg": pkg_name,
                 "topic": topic
             }
-
-        hh_links = {}
-
-        regions = v.get_regions("_hh_links")
-        for idx, region in enumerate(regions):
-            hh_links[str(region.begin())] = tmp[idx]
 
         v.settings().set("_hh_links", hh_links)
 
@@ -171,6 +165,7 @@ class HyperhelpInternalFlagLinksCommand(sublime_plugin.TextCommand):
     underlying help indexes may have changed, such as at Sublime startup.
     """
     def run(self, edit):
+        return
         v = self.view
         hh_links = v.settings().get("_hh_links")
 
