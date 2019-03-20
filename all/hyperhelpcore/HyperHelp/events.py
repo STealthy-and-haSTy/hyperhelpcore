ae40bcf7159a05c307f453c25e954281c43bbad4
Terence Martin <github@nurdz.com>
2019-03-20 13:29:52 -0700

    Enhance context handling with expanded universe
    In order to be more generally useful, as well as to provide a layer of
    insulation should we decide to change up the syntaxes, we have now
    implemented a set of contexts to be used in place of the single one
    that previously existed.
    
    These allow us to check things like whether a file is help source or not
    without having to verify with selectors, and also includes an enhanced
    set such as being able to determine if help is open at all and, if so,
    what package it's currently displaying.

--- all/hyperhelpcore/HyperHelp/events.py
+++ all/hyperhelpcore/HyperHelp/events.py
@@ -148,24 +148,6 @@ def _show_popup(view, point, popup):
 
 
 class HyperhelpEventListener(sublime_plugin.EventListener):
-    def on_query_context(self, view, key, operator, operand, match_all):
-        """
-        Provide custom key binding contexts for binding keys in hyperhelp
-        views.
-        """
-        if key == "hyperhelp.is_authoring":
-            lhs = view.is_read_only() == False
-            rhs = bool(operand)
-        else:
-            return None
-
-        if operator == sublime.OP_EQUAL:
-            return lhs == rhs
-        elif operator == sublime.OP_NOT_EQUAL:
-            return lhs != rhs
-
-        return None
-
     def on_text_command(self, view, command, args):
         """
         Listen for the drag_select command with arguments that tell us that the
